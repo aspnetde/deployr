71576e603184bc0c11079708a12c3413c8b7ab97
Marius Schulz <mariusschulz@users.noreply.github.com>
2012-06-14 16:54:03 +0200

    Refactoring + implemented image copy functionality

---
+++ DeployR/AgilityPackExtensions/NodeSelection/ImageNodeSelector.cs
@@ -0,0 +1,14 @@
+﻿using HtmlAgilityPack;
+
+namespace DeployR.AgilityPackExtensions.NodeSelection
+{
+    internal static class ImageNodeSelector
+    {
+        private const string ImagesXPath = "html//img";
+
+        public static HtmlNodeCollection SelectImageNodes(HtmlDocument htmlDocument)
+        {
+            return htmlDocument.DocumentNode.SelectNodes(ImagesXPath);
+        }
+    }
+}
--- DeployR/Exporting/StaticFileExporter.cs
+++ DeployR/Exporting/StaticFileExporter.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Web;
 using DeployR.AgilityPackExtensions.NodeSelection;
 using DeployR.FileGeneration;
 using DeployR.Web;
@@ -11,88 +13,112 @@ namespace DeployR.Exporting
     {
         private readonly HtmlDocument _htmlDocument;
         private ExportSettings _settings;
+        private readonly IDictionary<string, string> _copiedImagePaths;
 
         public StaticFileExporter(HtmlDocument htmlDocument)
         {
             _htmlDocument = htmlDocument;
+            _copiedImagePaths = new Dictionary<string, string>();
         }
 
         public void ExportFiles(ExportSettings settings)
         {
             _settings = settings;
 
-            DeleteAndRecreateDirectories();
+            DeleteAndRecreateDirectory();
 
             ExportCssFiles();
             ExportJavaScriptFiles();
+            ExportImagesHtml();
+
+            // The HTML file has to be exported last so that the HTML tags
+            // can be modified by the JavaScript, CSS, and image exporters
             ExportHtmlFile();
         }
 
-        private void DeleteAndRecreateDirectories()
+        private void DeleteAndRecreateDirectory()
         {
-            if (Directory.Exists(_settings.Android.TargetDirectory))
-            {
-                Directory.Delete(_settings.Android.TargetDirectory, recursive: true);
-            }
+            string[] files = Directory.GetFiles(_settings.TargetDirectory);
 
-            if (Directory.Exists(_settings.iOS.TargetDirectory))
+            foreach (string file in files)
             {
-                Directory.Delete(_settings.iOS.TargetDirectory, recursive: true);
+                File.Delete(file);
             }
-
-            Directory.CreateDirectory(_settings.Android.TargetDirectory);
-            Directory.CreateDirectory(_settings.iOS.TargetDirectory);
         }
 
         private void ExportHtmlFile()
         {
             string modifiedHtml = _htmlDocument.DocumentNode.OuterHtml;
+            string htmlFilePath = Path.Combine(_settings.TargetDirectory, _settings.HtmlFileName);
 
-            string htmlFilePathAndroid = Path.Combine(_settings.Android.TargetDirectory, FileNameSettings.HtmlFileName);
-            string htmlFilePathiOS = Path.Combine(_settings.iOS.TargetDirectory, FileNameSettings.HtmlFileName);
-
-            FileWriter.WriteFile(htmlFilePathAndroid, modifiedHtml);
-            FileWriter.WriteFile(htmlFilePathiOS, modifiedHtml);
+            FileWriter.WriteFile(htmlFilePath, modifiedHtml);
         }
 
         private void ExportCssFiles()
         {
-            
             HtmlNodeCollection stylesheetNodes = StylesheetNodeSelector.SelectStylesheetNodes(_htmlDocument);
 
             if (stylesheetNodes == null)
+            {
                 return;
+            }
 
             for (int i = 0; i < stylesheetNodes.Count; i++)
             {
                 HtmlNode node = stylesheetNodes[i];
-                
+
                 string href = node.Attributes["href"].Value;
                 string requestUri = RequestUriGenerator.GenerateUri(href);
                 string cssRules = FileContentRequestLoader.DownloadString(requestUri);
-                string fileName = string.Format(FileNameSettings.StylesheetFileNamePattern, i + 1);
+                string fileName = string.Format(_settings.StylesheetFileNamePattern, i + 1);
+                string stylesheetfilePath = Path.Combine(_settings.TargetDirectory, fileName);
 
-                string stylesheetfilePathAndroid = Path.Combine(_settings.Android.TargetDirectory, fileName);
-                string stylesheetfilePathiOS = Path.Combine(_settings.iOS.TargetDirectory, fileName);
-
-                FileWriter.WriteFile(stylesheetfilePathAndroid, cssRules);
-                FileWriter.WriteFile(stylesheetfilePathiOS, cssRules);
+                cssRules = ReplaceCssImageReferences(cssRules);
+                FileWriter.WriteFile(stylesheetfilePath, cssRules);
 
                 HtmlNode newNode = _htmlDocument.CreateElement("link");
                 newNode.CopyFrom(node);
                 newNode.Attributes["href"].Value = fileName;
                 node.ParentNode.ReplaceChild(newNode, node);
             }
+        }
 
+        private string ReplaceCssImageReferences(string cssRules)
+        {
+            const string referencedImagesPattern = @"(?<=url\("")(?<path>(?:.+?)\.(?<extension>[a-z]+))(?=""\))";
+            cssRules = Regex.Replace(cssRules, referencedImagesPattern, CopyImageAndUpdateReference);
+
+            return cssRules;
+        }
+
+        private string CopyImageAndUpdateReference(Match match)
+        {
+            string filePath = match.Groups["path"].Value;
+
+            if (_copiedImagePaths.Keys.Contains(filePath))
+            {
+                return _copiedImagePaths[filePath];
+            }
+
+            string fileExtension = match.Groups["extension"].Value;
+            string newFileName = string.Format(_settings.ImageFileNamePattern, _copiedImagePaths.Count + 1, fileExtension);
+            string imageSourcePath = HttpContext.Current.Server.MapPath(filePath);
+            string imageDestinationPath = Path.Combine(_settings.TargetDirectory, newFileName);
+
+            _copiedImagePaths.Add(filePath, newFileName);
+            File.Copy(imageSourcePath, imageDestinationPath, overwrite: true);
+
+            return newFileName;
         }
 
         private void ExportJavaScriptFiles()
         {
-
             HtmlNodeCollection javaScriptNodes = JavaScriptNodeSelector.SelectJavaScriptNodes(_htmlDocument);
 
             if (javaScriptNodes == null)
+            {
                 return;
+            }
 
             for (int i = 0; i < javaScriptNodes.Count; i++)
             {
@@ -100,24 +126,51 @@ namespace DeployR.Exporting
 
                 string src = node.Attributes["src"].Value;
                 string requestUri = RequestUriGenerator.GenerateUri(src);
-
-                Trace.TraceWarning("JS: " + requestUri);
-
                 string javaScriptFileContent = FileContentRequestLoader.DownloadString(requestUri);
-                string fileName = string.Format(FileNameSettings.JavaScriptFileNamePattern, i + 1);
+                string fileName = string.Format(_settings.JavaScriptFileNamePattern, i + 1);
+                string javaScriptFilePath = Path.Combine(_settings.TargetDirectory, fileName);
 
-                string javaScriptFilePathAndroid = Path.Combine(_settings.Android.TargetDirectory, fileName);
-                string javaScriptFilePathiOS = Path.Combine(_settings.iOS.TargetDirectory, fileName);
-
-                FileWriter.WriteFile(javaScriptFilePathAndroid, javaScriptFileContent);
-                FileWriter.WriteFile(javaScriptFilePathiOS, javaScriptFileContent);
+                FileWriter.WriteFile(javaScriptFilePath, javaScriptFileContent);
 
                 HtmlNode newNode = _htmlDocument.CreateElement("script");
                 newNode.CopyFrom(node);
                 newNode.Attributes["src"].Value = fileName;
                 node.ParentNode.ReplaceChild(newNode, node);
             }
+        }
 
+        private void ExportImagesHtml()
+        {
+            HtmlNodeCollection imageNodes = ImageNodeSelector.SelectImageNodes(_htmlDocument);
+
+            if (imageNodes == null)
+            {
+                return;
+            }
+
+            foreach (HtmlNode node in imageNodes)
+            {
+                string src = node.Attributes["src"].Value;
+                
+                if (_copiedImagePaths.Keys.Contains(src))
+                {
+                    continue;
+                }
+                
+                string imageUri = Path.Combine(_settings.TargetDirectory, src);
+                string imageSourcePath = HttpContext.Current.Server.MapPath(imageUri);
+                string extension = Path.GetExtension(imageSourcePath).TrimStart('.');
+                string newFileName = string.Format(_settings.ImageFileNamePattern, _copiedImagePaths.Count + 1, extension);
+                string imageDestinationPath = Path.Combine(_settings.TargetDirectory, newFileName);
+
+                _copiedImagePaths.Add(src, newFileName);
+                File.Copy(imageSourcePath, imageDestinationPath, overwrite: true);
+
+                HtmlNode newNode = _htmlDocument.CreateElement("img");
+                newNode.CopyFrom(node);
+                newNode.Attributes["src"].Value = newFileName;
+                node.ParentNode.ReplaceChild(newNode, node);
+            }
         }
     }
 }
